68128db29e25958e2e37ae0cef1d45c21e9d9151
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-05-29 21:43:12 -0500

    Control-Props-Alt Ok

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -1,51 +1,39 @@
 import { useState } from "react";
 import { Product, ProductInCart } from "../interfaces/interfaces";
 
-
 export const useShoppingCart = () => {
+  const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>(
+    {}
+  );
 
-  const [ shoppingCart, setShoppingCart ] = useState<{ [ key: string ]: ProductInCart }>( {} );
-
-  const onProductCountChange = ( { count, product }: { count: number, product: Product } ) => {
+  const onProductCountChange = ({
+    count,
+    product,
+  }: {
+    count: number;
+    product: Product;
+  }) => {
     // console.log( count, product );
-    console.log( { count } );
+    console.log({ count });
 
-    setShoppingCart( oldShoppingCart => {
-
-      const productInCart: ProductInCart = oldShoppingCart[ product.id ] || { ...product, count: 0 };
-
-      if ( Math.max( productInCart.count + count, 0 ) > 0 ) {
-        productInCart.count += count;
+    setShoppingCart((oldShoppingCart) => {
+      if (count === 0) {
+        const { [product.id]: todelete, ...rest } = oldShoppingCart;
 
         return {
-          ...oldShoppingCart,
-          [ product.id ]: productInCart,
-        }
+          ...rest,
+        };
+      }
+
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
       };
-
-      // Borrar el producto
-
-      const { [ product.id ]: todelete, ...rest } = oldShoppingCart;
-      return { ...rest, };
-
-      // if ( count === 0 ) {
-        
-      // const { [ product.id ]: todelete, ...rest } = oldShoppingCart;
-
-      // return {
-      //   ...rest
-      // };
-      // };
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [ product.id ]: { ...product, count },
-      // };
-    } );
-  }
+    });
+  };
 
   return {
     shoppingCart,
     onProductCountChange,
-  }
-}
+  };
+};
