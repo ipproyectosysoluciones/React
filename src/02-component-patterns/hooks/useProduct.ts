6918db769e5a3599b64db55d58d9c87cb8414268
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-05-29 21:27:58 -0500

    Control-Props Implemented

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { onChangeArgs, Product } from "../interfaces/interfaces";
 
-const useProduct = () => {
-  const [ counter, setCounter ] = useState( 0 );
+interface useProductArgs {
+  product: Product;
+  onChange?: ( arg: onChangeArgs ) => void;
+  value?: number;
+}
+
+const useProduct = ( { onChange, product, value = 0 }: useProductArgs ) => {
+
+  const [ counter, setCounter ] = useState( value );
+
+  const isControlled = useRef( !!onChange );
 
   const increaseBy = ( value: number ) => {
-    setCounter( prev => Math.max( prev + value, 0 ) );
+
+    // console.log( 'isControlled', isControlled.current );
+    if ( isControlled.current ) {
+      return onChange!({ count: value, product });
+    }
+
+    const newValue = Math.max( counter + value, 0 );
+    setCounter( newValue );
+
+    onChange && onChange({ count: newValue, product });
   }
+
+  useEffect(() => {
+    setCounter( value );
+  }, [ value ])
+  
 
   return { counter, increaseBy };
 }
